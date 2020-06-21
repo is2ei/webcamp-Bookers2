07ac2a740e841cfd0271d9e67481d8d7d72df7a8
Issei Horie <issei.horie@is2ei.com>
2020-06-21 23:01:01 +0900

    Added devise controllers
    Signed-off-by: Issei Horie <issei.horie@is2ei.com>

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_06_21_121645) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
     t.string "name", null: false
     t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
