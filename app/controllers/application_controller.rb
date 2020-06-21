85a0716ad67552f3808cb8078cd20798215815f1
Issei Horie <issei.horie@is2ei.com>
2020-06-21 22:44:57 +0900

    Signup feature
    Signed-off-by: Issei Horie <issei.horie@is2ei.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, except: [:index, :about]
 
   def index
   end
 
   def about
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
