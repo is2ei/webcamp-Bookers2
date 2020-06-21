87a638b9046823ec1e8587a5e7876aad068e91da
Issei Horie <issei.horie@is2ei.com>
2020-06-21 22:29:28 +0900

    Added header
    Signed-off-by: Issei Horie <issei.horie@is2ei.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :about]
+
+  def index
+  end
+
+  def about
+  end
+
 end
