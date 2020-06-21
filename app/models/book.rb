c23da5f3da1cd9391ce7c24a977b1b29ccc639e5
Issei Horie <issei.horie@is2ei.com>
2020-06-21 21:44:28 +0900

    Added validation to models
    Signed-off-by: Issei Horie <issei.horie@is2ei.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
+  validates :title, presence: true, allow_blank: false
+  validates :body, presence: true, allow_blank: false, length: { maximum: 200 }
   belongs_to :user
 end
