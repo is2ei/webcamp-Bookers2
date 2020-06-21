b097a6cc18b2ebbeaba07bea328ffa5d068aca61
Issei Horie <issei.horie@is2ei.com>
2020-06-21 21:38:16 +0900

    Added model book
    Signed-off-by: Issei Horie <issei.horie@is2ei.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_114345) do
+ActiveRecord::Schema.define(version: 2020_06_21_121645) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_06_21_114345) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "books", "users"
 end
